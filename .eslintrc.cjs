4a8bfce8e96c9398261e72baea491bfef5433882
abdelhafiz eltaher <abdelhafiz.eltaher.98@gmai.com>
2024-08-21 02:44:23 +0300

    setUp Project

---
+++ .eslintrc.cjs
@@ -0,0 +1,83 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.js'],
+      parser: '@typescript-eslint/parser',
+    },
+  ],
+  ignorePatterns: [
+    'dist',
+    '.eslintrc.cjs',
+    'tailwind.config.ts',
+    'vite.config.ts',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier',
+    'react-refresh',
+    '@tanstack/query',
+  ],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/react-in-jsx-scope': 0,
+    'max-lines-per-function': ['error', 500],
+    'react/no-string-refs': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/require-default-props': 'off',
+    '@tanstack/query/exhaustive-deps': 'error',
+    '@tanstack/query/prefer-query-object-syntax': 'error',
+    'linebreak-style': 'off',
+    'no-console': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 0,
+    'tailwindcss/no-custom-classname': 0,
+    'react/button-has-type': 0,
+    'object-curly-newline': 0,
+    'operator-linebreak': 0,
+    '@typescript-eslint/indent': 0,
+    'react/jsx-wrap-multilines': 0,
+    // 'no-nested-ternary': 0,
+    'function-paren-newline': 0,
+    'react/jsx-curly-newline': 0,
+    'prefer-arrow-callback': 0,
+    'import/no-cycle': 0,
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id'],
+        },
+      },
+    ],
+    'jsx-a11y/label-has-for': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id'],
+        },
+      },
+    ],
+    'implicit-arrow-linebreak': 0,
+    'react/jsx-one-expression-per-line': 0,
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3571,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@modules': path.resolve(__dirname, './src/modules'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@apis': path.resolve(__dirname, './src/apis'),
+      '@domains': path.resolve(__dirname, './src/domains'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@libs': path.resolve(__dirname, './src/libs'),
+      '@routing': path.resolve(__dirname, './src/routing'),
+      '@store': path.resolve(__dirname, './src/store'),
+    },
+    preserveSymlinks: true,
+  },
+});
