7a42185607ddc7171d796a59c3134cffe5e6cd9b
GhadeerAbed <ghadfannana@gmail.com>
2024-12-17 19:08:28 +0200

    update

--- src/modules/auth/index.tsx
+++ src/modules/auth/index.tsx
@@ -41,7 +41,7 @@ export default function AuthPage() {
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="flex items-center mb-6 text-3xl font-meduim text-gray-900 ">
           <img className="w-8 h-8 mr-2" src="EnjoySpot_Icon.svg" alt="logo" />
-          Enjoy Spot CMS
+          Enjoy Spot CMS hello
         </div>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 ">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
